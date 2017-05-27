d872da8979f274b07c59c09551e31e902431ad54
Aaron Allen <aaronallen8455@gmail.com>
2017-05-27 07:07:22 -0500

    improved commerce

--- modules/custom/store_front/src/Controller/StoreController.php
+++ modules/custom/store_front/src/Controller/StoreController.php
@@ -12,8 +12,9 @@ namespace Drupal\store_front\Controller;
 use Drupal\commerce_product\ProductLazyBuilders;
 use Drupal\commerce_product\ProductVariationFieldRendererInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Image\ImageFactory;
 use Drupal\Core\Render\Markup;
-use Kint;
+use Drupal\file\Entity\File;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class StoreController extends ControllerBase {
@@ -24,18 +25,26 @@ class StoreController extends ControllerBase {
   /** @var  \Drupal\commerce_product\ProductVariationFieldRendererInterface */
   protected $variation_field_renderer;
 
+  /** @var \Drupal\Core\Image\ImageFactory  */
+  protected $image_factory;
+
   public static function create(ContainerInterface $container) {
 
     $product_lazy_builder = $container->get('commerce_product.lazy_builders');
-
     $product_varient_field_renderer = $container->get('commerce_product.variation_field_renderer');
+    $image_factory = $container->get('image.factory');
 
-    return new static($product_lazy_builder, $product_varient_field_renderer);
+    return new static($product_lazy_builder, $product_varient_field_renderer, $image_factory);
   }
 
-  public function __construct(ProductLazyBuilders $product_lazy_builder, ProductVariationFieldRendererInterface $field_renderer) {
+  public function __construct(
+    ProductLazyBuilders $product_lazy_builder,
+    ProductVariationFieldRendererInterface $field_renderer,
+    ImageFactory $image_factory
+  ) {
     $this->product_builder = $product_lazy_builder;
     $this->variation_field_renderer = $field_renderer;
+    $this->image_factory = $image_factory;
   }
 
   public function content() {
@@ -54,32 +63,39 @@ class StoreController extends ControllerBase {
       /** @var \Drupal\commerce_product\Entity\ProductInterface $product */
       $product = $storage->load($id);
       $variants = $product->getVariations();
-      $url = $variants[0]->toUrl()->getInternalPath();
-      //$this->ksm($product->get('body')->getValue());
+      //$url = $variants[0]->toUrl()->getInternalPath();
+
+      // build the image render array
+      $imgField = $product->get('field_image')->getValue()[0];
+      $imageArray = array();
+      if (!empty($imgField)) {
+        $file = File::load($imgField['target_id']);
+        $image = $this->image_factory->get($file->getFileUri());
+        if ($image->isValid()) {
+          $imageArray = [
+            '#theme' => 'image_style',
+            '#width' => $image->getWidth(),
+            '#height' => $image->getHeight(),
+            '#style_name' => 'large',
+            '#uri' => $file->getFileUri(),
+          ];
+        }
+      }
+
       $product_forms[$id . '_openTag']['#markup'] = '<div class="product-list-item">';
-      $product_forms[$id . '_title']['#markup'] = "<a href='$url'>" . $product->getTitle() . "</a>";
-      $product_forms[$id . '_price'] = $this->variation_field_renderer->renderField('price', $variants[0]);
+      $product_forms[$id . '_title']['#markup'] = "<h1>" . $product->getTitle() . "</h1>";
+      if (!empty($imageArray)) {
+        $product_forms[$id . '_image'] = $imageArray;
+      }
       $product_forms[$id . '_body']['#markup'] = $product->get('body')->getValue()[0]['value'];
+      $product_forms[$id . '_price'] = $this->variation_field_renderer->renderField('price', $variants[0]);
       $product_forms[$id . '_form'] = $this->product_builder->addToCartForm($id, 'default', false);
       $product_forms[$id . '_closeTag']['#markup'] = '</div>';
     }
 
-    //$product_forms['test'] = ['#markup' => 'TEST'];
-    //$this->ksm($product_forms);
+    // add css library
+    $product_forms[] = ['#attached' => ['library' => ['store_front/store_front']]];
 
     return $product_forms;
-  }
-
-  public function ksm() {
-    $this->kint_require();
-    if (\Drupal::currentUser()->hasPermission('access kint')) {
-      $args = func_get_args();
-      $msg = @Kint::dump($args);
-      drupal_set_message(Markup::create($msg));
-    }
-  }
-
-  public function kint_require() {
-    return require_once DRUPAL_ROOT . '/' . drupal_get_path('module', 'kint') . '/kint/Kint.class.php';
   }
 }
